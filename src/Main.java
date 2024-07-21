90cb3ef9a37c490733451989107e59bdde944b51
Шипунов Владимир Михайлович (4050213) <vshipunov@vtb.ru>
2024-07-21 17:18:15 +0400

    Add file path parser

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,27 @@
 package src;
 
+import java.io.File;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-        String text = new Scanner(System.in).nextLine();
-        System.out.println("Длина текста: " + text.length());
+        filesCheck();
+    }
+
+    public static void filesCheck() {
+        for (int i = 1; ; ) {
+            String path = new Scanner(System.in).nextLine();
+            File file = new File(path);
+            boolean isFile = file.isFile();
+            boolean isDirectory = file.isDirectory();
+            if (isFile == false || isDirectory == true) {
+                System.out.println("Данный путь не содержит файла или ведёт к папке");
+            } else {
+                System.out.println("Путь указан верно");
+                System.out.println("Это файл номер " + i);
+                i++;
+            }
+        }
     }
 }
